832c0330c4e29591af69b5d8c07116ea932e1235
Andrei Rohau <arohau@edmunds.com>
2025-11-05 19:17:23 +0300

    init

---
+++ module-2-task/src/test/java/jar/AppTest.java
@@ -0,0 +1,15 @@
+package jar;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+//    @Test
+    public void shouldAnswerWithTrue() {
+        //
+    }
+}
